da10e2298007665edacecb7905c05be2e15ec5d7
Param Raghavan <paramraghavan@gmail.com>
2023-11-20 11:43:52 -0500

    Bump version: 0.1.0 → 0.1.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ if os.path.isfile(requirement_path):
 
 setup(
     name='mypackage',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(where="src"), # packages=["src"],
     package_dir={"": "src"},
     # install_requires=[
